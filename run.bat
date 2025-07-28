2206a3162a7e38413ccfb72a78ba0acced344b62
spagettot <gmedaminee@gmail.com>
2025-07-28 18:44:02 +0100

    fixe main.exe probleme

--- run.bat
+++ run.bat
@@ -9,7 +9,7 @@ for %%f in (src\*.c) do (
 echo compiling ....
 
 gcc -O3 -Iinclude -Llib !SOURCES! -o pong.exe -lraylib -lopengl32 -lgdi32 -lwinmm -mwindows
-start main.exe
+start pong.exe
 
 echo,
 echo compilation finished
